904728af299f12ed51e05fe9e8513e776f5e27c6
Cody <dakotalrubin@gmail.com>
2022-09-08 08:46:20 -0700

    Add capitalize() helper function and getComputerChoice() function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+// This helper function formats user input strings into standard options
+// that the game logic will recognize: "Rock", "Paper", and "Scissors".
+function capitalize(string) {
+    let lowerCaseString = string.toLowerCase();
+    let firstChar = lowerCaseString[0].toUpperCase();
+
+    return firstChar + lowerCaseString.slice(1);
+}
+
+// This function returns the computer's random selection from the options
+// "Rock", "Paper", and "Scissors".
+function getComputerChoice() {
+    let computerChoice = "";
+    let randomNumber = Math.floor((Math.random() * 3) + 1);
+
+    if (randomNumber === 1) {
+        computerChoice = "Rock";
+    } else if (randomNumber === 2) {
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissors";
+    }
+
+    return computerChoice;
+}
